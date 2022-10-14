41e3c69499f1600cdca510012ea3e54542e7aa35
kero-emad2003 <keroemadsaad2019@gmail.com>
2022-10-14 16:03:36 +0200

    creating an obj to test git and github

---
+++ Project1/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter two numbers" << endl;
+	int a, b;
+	cin >> a >> b;
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+	cout << a << " % " << b << " = " << a % b << endl;
+
+
+}
